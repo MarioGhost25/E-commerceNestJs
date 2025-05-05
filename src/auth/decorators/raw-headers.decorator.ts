275cf423924089dda26dd7f13c5f99b8d5fa2c94
Miembro-C <mariomejia2505@gmail.com>
2025-05-05 16:28:43 -0600

    moving @nest/passport to dependecies due to coolify doesn't work for it

--- src/auth/decorators/raw-headers.decorator.ts
+++ src/auth/decorators/raw-headers.decorator.ts
@@ -3,8 +3,6 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
 export const GetRawHeaders = createParamDecorator(
     ( data: string, ctx: ExecutionContext ) => {
-        
-
         const req = ctx.switchToHttp().getRequest();
         return req.rawHeaders;
     }
